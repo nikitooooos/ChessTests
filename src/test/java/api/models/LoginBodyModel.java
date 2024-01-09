b6b84fef2528e283fb2f76016f2d5f1e95029d25
nikitooooos <nikita.postnikov.00@gmail.com>
2024-01-09 20:31:59 +0300

    delite api

--- src/test/java/api/models/LoginBodyModel.java
+++
@@ -1,9 +0,0 @@
-package api.models;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public @Data class LoginBodyModel {
-    String email, password;
-}
